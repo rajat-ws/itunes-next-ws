93a65256454c0fcaf8f46182311c28eb21cdfe1c
rajat-ws <rajat.mehra@wednesday.is>
2022-11-01 16:06:32 +0530

    chore: remove unnecessary tests

--- features/itunes/components/TracksList/tests/index.test.tsx
+++ features/itunes/components/TracksList/tests/index.test.tsx
@@ -22,36 +22,9 @@ describe("<TracksList />", () => {
     loading: true,
   };
 
-  const trackCardProps = {
-    data: {
-      artistName: "Adele",
-      trackName: "Hello",
-      trackId: 1544494392,
-      trackTimeMillis: 295502,
-      artworkUrl100:
-        "https://is5-ssl.mzstatic.com/image/thumb/Music116/v4/5a/47/6d/5a476ddd-4690-1297-1896-6a286a497a21/191404113974.png/100x100bb.jpg",
-      previewUrl: "22233223",
-      collectionName: "collecition",
-      country: "India",
-      primaryGenreName: "abc",
-      kind: "Kind",
-      wrapperType: "Rap",
-    },
-    isShowDetailsButton: false,
-    isShowDetails: true,
-  };
-
   it("should render and match the snapshot", () => {
     const { baseElement } = render(<TracksList {...trackListDataProps} />);
     expect(baseElement).toMatchSnapshot();
-  });
-
-  it("should check if Track Card exists in TrackList", () => {
-    const handlePlayPauseWrapperSpy = jest.fn();
-    const { getByTestId } = render(
-      <TrackCard {...trackCardProps} handlePlayPauseWrapper={handlePlayPauseWrapperSpy} />
-    );
-    expect(getByTestId("track-card")).toBeInTheDocument();
   });
 
   it("should render the Pause text when Play button is clicked", async () => {
