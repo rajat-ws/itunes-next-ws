7ec904ce1ff43a2c43c2f23853d9891f40625a7e
rajat-ws <rajat.mehra@wednesday.is>
2022-11-02 11:06:14 +0530

    chore: changed tracknames and URl for recommendations

--- services/root.ts
+++ services/root.ts
@@ -10,14 +10,14 @@ const itunesApi = generateApiClient(APIClients.ITUNES);
 
 export const getRecommendations = async () => {
   if (!itunesApi) return [];
-  const res = await itunesApi.get<TrackDetail>("/search?term=coldplay");
+  const res = await itunesApi.get<TrackDetail>("/search?term=anuv");
 
   const getData = (response: ApiResponse<TrackDetail>) => {
     if (!response.ok || !response.data) {
       return [];
     }
 
-    const recommendations = ["Magic", "Yellow"];
+    const recommendations = ["Ocean", "Gul"];
 
     return response.data.results
       .filter(({ trackName }) => recommendations.includes(trackName))
